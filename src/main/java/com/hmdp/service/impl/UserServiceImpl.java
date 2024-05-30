192160691bbd7ec11de46e7a918dfd7a54e8e119
Roiben <roiben@163.com>
2024-05-30 15:55:25 +0800

    发送验证码

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,17 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexPatterns;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -15,6 +22,23 @@ import org.springframework.stereotype.Service;
  * @since 2021-12-22
  */
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // 检验手机号
+        if (RegexUtils.isPhoneInvalid(phone)){
+            // 不符合返回错误信息
+            return Result.fail("手机号格式错误");
+        }
+        // 生成验证码
+        String code = RandomUtil.randomNumbers(6);
+        // 保存验证码到session
+        session.setAttribute("code", code);
+        // TODO 发送短信验证码
+        log.info("发送验证码：{}", code);
+        return Result.ok();
+
+    }
 }
