2290bbe140a3e74b58230edcf4d800c4f1a45c0e
Y-Roiben <1194730148@qq.com>
2024-05-30 20:05:01 +0800

    用户登录

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
@@ -12,6 +13,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -36,9 +39,47 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         String code = RandomUtil.randomNumbers(6);
         // 保存验证码到session
         session.setAttribute("code", code);
-        // TODO 发送短信验证码
+        //  发送短信验证码
         log.info("发送验证码：{}", code);
         return Result.ok();
 
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        // 检验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            // 不符合返回错误信息
+            return Result.fail("手机号格式错误");
+        }
+        // 检验验证码
+        Object cacheCode = session.getAttribute("code");
+        log.info("cacheCode:{}", cacheCode);
+        String code = loginForm.getCode();
+        // 不一致, 报错
+        if (cacheCode == null || !cacheCode.toString().equals(code)) {
+            return Result.fail("验证码错误");
+        }
+        // 一致, 根据手机号查询用户信息
+        User user = query().eq("phone", phone).one();
+        // 判断用户是否存在
+        if (user == null) {
+            // 不存在, 创建用户
+            user = createUserByPhone(phone);
+        }
+
+        // 保存用户信息到session
+        session.setAttribute("user", user);
+        return Result.ok();
+    }
+    private User createUserByPhone (String phone){
+        // 创建用户
+        User user = new User();
+        user.setPhone(phone);
+        String nickName = USER_NICK_NAME_PREFIX + RandomUtil.randomString(10);
+        user.setNickName(nickName);
+        save(user);
+        return user;
     }
 }
